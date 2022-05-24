f90ac1453766bb81ddd17eee45f330a25bcf4c0e
stysiok <stysiok@o2.pl>
2022-05-24 21:03:26 +0200

    Add colleration and yarp to project

--- src/YAMI.Gateway/Program.cs
+++ src/YAMI.Gateway/Program.cs
@@ -1,6 +1,21 @@
+using Yarp.ReverseProxy.Transforms;
+using YAMI.Common.Colleration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"))
+    .AddTransforms(builderContext =>
+    {
+        builderContext.AddRequestTransform(transformContext =>
+        {
+            transformContext.ProxyRequest.Headers.AddCollerationId(Guid.NewGuid());
+            return ValueTask.CompletedTask;
+        });
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapReverseProxy();
 app.Run();
---
+++ src/shared/YAMI.Common/Colleration/CollerationExtensions.cs
@@ -0,0 +1,30 @@
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace YAMI.Common.Colleration;
+
+public static class CollerationExtensions
+{
+    private const string COLLERATION_ID_KEY = "colleration-id";
+
+    public static IApplicationBuilder UseColleration(this IApplicationBuilder app)
+        => app.Use(async (context, next) =>
+        {
+            if (!context.Request.Headers.TryGetValue(COLLERATION_ID_KEY, out var collerationId))
+                collerationId = Guid.NewGuid().ToString("N");
+            
+            context.Items[COLLERATION_ID_KEY] = collerationId.ToString();
+
+            await next();
+        });
+    
+    public static string? GetCollerationId(this HttpContext context)
+        => context.Items.TryGetValue(COLLERATION_ID_KEY, out var collerationId) ? collerationId as string : null;
+
+    public static void AddCollerationId(this HttpRequestHeaders headers, string collerationId)
+        => headers.TryAddWithoutValidation(COLLERATION_ID_KEY, collerationId);
+
+    public static void AddCollerationId(this HttpRequestHeaders headers, Guid collerationId)
+        => headers.AddCollerationId(collerationId.ToString("N"));
+}
